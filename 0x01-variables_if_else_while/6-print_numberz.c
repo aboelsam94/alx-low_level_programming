d764216c595711fe15fb7fe3b29b82899b95f229
Mohamed Samy <themohamedsamy@gmail.com>
2023-06-16 18:57:45 -0700

    z

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Description: print numbersz
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + '0');
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
