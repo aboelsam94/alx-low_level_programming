03a24b16dda1b253f2d3ba99fa47c64e07af04c2
Mohamed Samy <themohamedsamy@gmail.com>
2023-06-21 07:31:39 -0700

    s

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: computes the sum of all
+ *              multiple of 3 and 5
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int sum, num;
+
+	for (num = 0; num < 1024; num++)
+	{
+		if ((num % 3 == 0) || (num % 5 == 0))
+			sum += num;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
