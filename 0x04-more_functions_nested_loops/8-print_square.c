b671d9747da2019741f1e30f13c77058d1733869
Mohamed Samy <themohamedsamy@gmail.com>
2023-06-23 02:22:30 +0000

    s

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_square - print a suqare using the # sign
+ *
+ * @size: is the size of the square
+ *
+ * Return: 0 (Success)
+*/
+
+void print_square(int size)
+{
+	int row, column;
+
+	for (row = 1; row <= size; row++)
+		_putchar('#');
+	_putchar('\n');
+}
