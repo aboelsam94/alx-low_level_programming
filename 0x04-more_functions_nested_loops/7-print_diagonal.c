a1e592880252562d372e8711740531ca4ea24768
Mohamed Samy <themohamedsamy@gmail.com>
2023-06-23 03:10:33 +0000

    s

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,15 +11,13 @@ void print_diagonal(int n)
 	int postn, space;
 
 	if (n <= 0)
-		_putchar('\n');
-	else
+		return;
+
+	for (postn = 1; postn <= n; postn++)
 	{
-		for (postn = 1; postn <= n; postn++)
-		{
-			for (space = 0; space < postn; space++)
-				_putchar(' ');
-			_putchar(92);
-			_putchar('\n');
-		}
+		for (space = 1; space < postn; space++)
+			_putchar(' ');
+		_putchar(92);
+		_putchar('\n');
 	}
 }
