65763c60b4dfa9ecd39bd2954075cd1d4dbd70bf
Mohamed Samy <themohamedsamy@gmail.com>
2023-06-30 01:36:20 +0000

    finish task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * rot13 - a function that encodes a string using rot13
@@ -13,8 +14,8 @@ char *rot13(char *s)
 	int i;
 	int j;
 
-	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char ROT13[] = "nopqrstuvwxyzabcsdefhijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char rot13[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char ROT13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
