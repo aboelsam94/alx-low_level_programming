1661b9c413a4ee8c8826ac53a92d514f265bc2cb
Mohamed Samy <themohamedsamy@gmail.com>
2023-07-03 21:34:18 +0000

    finish task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,4 +2,25 @@
 
 /**
  * _strstr - a function that locates a substring.
- * */
+ * @haystack: input.
+ * @needle: input.
+ * Return: Alwways (0)
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *p = needle;
+
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+		if (*p == '\0')
+			return (haystack);
+	}
+	return (0);
+}
