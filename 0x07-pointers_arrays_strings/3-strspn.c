0e2d5f1253a8b66e95c7cb3d152f2e0359f7cce8
Mohamed Samy <themohamedsamy@gmail.com>
2023-07-03 20:24:32 +0000

    finish task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,7 +13,7 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (x = 0; s[x] != '\0'; x++)
 	{
-		for (y = 0; accept[y] != s[y]; y++)
+		for (y = 0; accept[y] != s[x]; y++)
 		{
 			if (accept[y] == '\0')
 				return (x);
