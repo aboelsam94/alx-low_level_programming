fafa213714f9d27dcaa113fae1c00cf486061030
Mohamed Samy <themohamedsamy@gmail.com>
2023-07-03 21:51:25 +0000

    finish task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -21,7 +21,7 @@ void print_diagsums(int *a, int size)
 	}
 	for (x = size - 1; x >= 0; x--)
 	{
-		sum2 += a[y * size + (size - y - 1)];
+		sum2 += a[x * size + (size - x - 1)];
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
