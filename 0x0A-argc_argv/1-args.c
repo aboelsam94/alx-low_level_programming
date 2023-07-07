44ed140d554efea6cbbb6271767c38a8f30e77d2
Mohamed Samy <themohamedsamy@gmail.com>
2023-07-07 19:03:07 +0000

    finish task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints the number of arguments passed into it.
+ * @argc: number of arguments.
+ * @argv: pointer to an array of arguments.
+ * Return: 0 on success, and non zero on failure.
+*/
+
+int main(int argc, char *argv[] __attribute__(unused))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
