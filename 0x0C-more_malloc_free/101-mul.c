fbfb8cc2333c945f7ace3620ddfbfb22eea50fea
Mohamed Samy <themohamedsamy@gmail.com>
2023-07-13 18:06:52 +0000

    finish task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -55,7 +55,7 @@ void print_int(unsigned long int n)
 	unsigned long int divisor = 1, i, resp;
 
 	for (i = 0; n / divisor > 9; i++, divisor *= 10)
-		;
+	;
 	for (; divisor >= 1; n %= divisor, divisor /= 10)
 	{
 		resp = n / divisor;
