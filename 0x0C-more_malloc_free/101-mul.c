7ba8170a8e28d429a33f2678d0083288ecc35c25
Mohamed Samy <themohamedsamy@gmail.com>
2023-07-13 20:28:01 +0000

    finish task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,119 +1,120 @@
 #include "main.h"
+#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
+#include <ctype.h>
 
 /**
- * _isdigit - checks if char is digit.
- * @c: the char to be checked.
- * Return: 1 if digit, and 0 if not.
+ * _is_zero - checks of any number is 0.
+ * @argv: argument vector.
+ * Return: void.
 */
 
-int _isdigit(int c)
+void _is_zero(char *argv[])
 {
-	return (c >= '0' && c <= '9');
-}
+	int i, isn1 = 1, isn2 = 1;
 
-/**
- * _strlen - returns the length of a string.
- * @s: thr string.
- * Return: length of string in an int.
-*/
-
-int _strlen(char *s)
-{
-	int x = 0;
-
-	while (*s++)
+	for (i = 0; argv[1][i]; i++)
+		if (argv[1][i] != '0')
 	{
-		x++;
+		isn1 = 0;
+		break;
 	}
-	return (x);
-}
-
-/**
- * big_num_multiply - multiply two nums.
- * @s1: first num.
- * @s2: second num.
- * Return: num string.
-*/
-
-char *big_num_multiply(char *s1, char *s2)
-{
-	char *y;
-	int x1, x2, a, b, c, z;
-
-	x1 = _strlen(s1);
-	x2 = _strlen(s2);
-
-	y = malloc(a = z = x1 + x2);
-	if (!y)
-		printf("Error\n"), exit(98);
-	while (a--)
-		y[a] = 0;
-
-	for (x1--; x1 >= 0; x1--)
+	for (i = 0; argv[2][i]; i++)
+		if (argv[2][i] != '0')
+		{
+			isn2 = 0;
+			break;
+		}
+	if (isn1 == 1 || isn2 == 1)
 	{
-		if (!_isdigit(s1[x1]))
-		{
-			free(y);
-			printf("Error\n"), exit(98);
-		}
-		a = s1[x1] - '0';
-		c = 0;
-
-		for (x2 = _strlen(s2) - 1; x2 >= 0; x2--)
-		{
-			if (!_isdigit(s2[x2]))
-			{
-				free(y);
-				printf("Error\n"), exit(98);
-			}
-			b = s2[x2] - '0';
-
-			c += y[x1 + x2 + 1] + (a * b);
-			y[x1 + x2 + 1] = c % 10;
-
-			c /= 10;
-		}
-		if (c)
-			y[x1 + x2 + 1] += c;
+		printf("0\n");
+		exit(0);
 	}
-	return (y);
 }
 
 /**
- * main - print the result of the multiplication.
- * @argc: int.
- * @argv: list.
- * Return: 0.
+ * _init_array - set memory to zero in a new array.
+ * @ar: char array.
+ * @lar: length of char array.
+ * Return: pointer to array.
 */
 
-int main(int argc, char const *argv[])
+char *_init_array(char *ar, int lar)
 {
-	char *z;
-	int a, b, c;
+	int i = 0;
+
+	for (i = 0; i < lar; i++)
+		ar[i] = '0';
+	ar[lar] = '\0';
+	return (ar);
+}
+
+/**
+ * _check_num - determines the length of num.
+ * @argv: argument vector.
+ * @n: row of the array.
+ * Return: length of the number.
+*/
+
+int _check_num(char *argv[], int n)
+{
+	int ln;
+
+	for (ln = 0; argv[n][ln]; ln++)
+		if (!isdigit(argv[n][ln]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+}
+
+/**
+ * main - entry point.
+ * @argc: num of arguments.
+ * @argv: arguments vector.
+ * Return: 0 on Success.
+*/
+
+int main(int argc, char *argv[])
+{
+	int ln1, ln2, lnout, add, add1, i, j, k, x;
+	char *nout;
 
 	if (argc != 3)
-	{
 		printf("Error\n"), exit(98);
-	}
-
-	c = _strlen(argv[1]) + _strlen(argv[2]);
-	z = big_num_multiply(argv[1], argv[2]);
-	b = 0;
-	a = 0;
-
-	while (b < c)
+	ln1 = _check_num(argv, 1), ln2 = _check_num(argv, 2), _is_zero(argv);
+	lnout = ln1 + ln2;
+	nout = malloc(lnout + 1);
+	if (nout == NULL)
+		printf("Error\n"), exit(98);
+	nout = _init_array(nout, lnout);
+	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, x = add1 = 0;
+	for (; k >= 0; k--, i--)
 	{
-		if (z[b])
-			a = 1;
-		if (a)
-			_putchar(z[c] + '0');
-		b++;
+		if (i < 0)
+		{
+			if (add1 > 0)
+
+				add = (nout[k] - '0') + add1;
+				if (add > 9)
+					nout[k - 1] = (add / 10) + '0';
+				nout[k] = (add % 10) + '0';
+
+			i = ln1 - 1, j--, add1 = 0, x++, k = lnout - (1 + x);
+		}
+		if (j < 0)
+
+			if (nout[0] != '0')
+				break;
+			lnout--;
+			free(nout), nout = malloc(lnout + 1), nout = _init_array(nout, lnout);
+			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, x = add1 = 0;
+
+		if (j >= 0)
+		{
+			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + add1;
+			add1 = add / 10, nout[k] = (add % 10) + '0';
+		}
 	}
-	if (!a)
-		_putchar('0');
-	_putchar('\n');
-	free(z);
-	return (0);
+	printf("%s\n", nout), return (0);
 }
