f7c9587d205cd93cabcc6138dc59228dbc4061b3
Mohamed Samy <themohamedsamy@gmail.com>
2023-07-13 17:11:22 +0000

    finish task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -35,7 +35,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		str[i] = s1[i];
 	for (j = 0; j < n && s2[j] != '\0'; j++)
 	{
-		str[i] = s2[i];
+		str[i] = s2[j];
 		i++;
 	}
 	str[i] = '\0';
