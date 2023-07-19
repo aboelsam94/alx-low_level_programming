3580afc6ea0e7e0aa8e297236455e382f47dffcf
Mohamed Samy <themohamedsamy@gmail.com>
2023-07-19 22:20:18 +0000

    finish task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,12 +6,13 @@
  * @name: the string name.
  * @f: the printing pointer func.
  *
- * Return: Void.
+ * Return: ivoid.
 */
 
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
+
 	f(name);
 }
