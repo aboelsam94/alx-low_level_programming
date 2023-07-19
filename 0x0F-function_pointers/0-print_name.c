fb9af13f48fe8dc75d64e83612ecdeead0ceaf83
Mohamed Samy <themohamedsamy@gmail.com>
2023-07-19 23:04:50 +0000

    finish task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -17,3 +17,28 @@ void print_name(char *name, void (*f)(char *))
 
 	f(name);
 }
+
+/**
+ * print_a_name - a func that prints a name.
+ * @name: name to be printed.
+ *
+ * Return: void.
+*/
+
+void print_a_name(char *name)
+{
+	printf("%s\n", name);
+}
+
+/**
+ * main - entet of the program.
+ *
+ * Return: 0.
+*/
+
+int main(void)
+{
+	print_name("Mo", print_a_name);
+
+	return (0);
+}
