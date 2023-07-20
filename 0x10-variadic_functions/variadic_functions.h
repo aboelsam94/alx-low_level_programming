472bd43ca10064e038bd983c1a6f4f7b7a851a27
Mohamed Samy <themohamedsamy@gmail.com>
2023-07-20 17:26:10 +0000

    finish task 0

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,13 @@
+#ifndef VARIADRIC_FUNCTIONS_H
+#define VARIADRIC_FUNCTIONS_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif /* VARIADRIC_FUNCTIONS_H */
