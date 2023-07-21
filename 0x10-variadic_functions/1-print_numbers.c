b8a4659f56b626e66f81b447761e40ac373c1658
Mohamed Samy <themohamedsamy@gmail.com>
2023-07-21 00:24:09 +0000

    finish task 1 & 2

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		return;
 	}
 	va_start(ap, n);
-	while (i--) 
+	while (i--)
 		printf("%d%s", va_arg(ap, int), i ? (separator ? separator : "") : "\n");
 	va_end(ap);
 }
